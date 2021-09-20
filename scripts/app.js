1089158bf66ab142b9d12f8ffb2b48a49394242c
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-09-20 15:16:08 -0600

    Added function to move obstacle left across game area and disappear object when it leaves game area

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-console.log("Hello Project");
+let increment = 0;
+function moveBlockLeft() {
+        
+    increment += 1;
+        console.log(increment);
+        $(".obstacle1").css({ marginRight: `${increment}px`, marginLeft: `${660-increment}px` }); //had difficulty with syntax of template literals in this case
+        //Trying to make block disappear when it leaves game area
+        if (increment >= 720) {
+            $(".obstacle1").hide();
+        }
+    }
+
+setInterval(moveBlockLeft, 10);
