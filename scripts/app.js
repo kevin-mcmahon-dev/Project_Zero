80984a481c00b9ebe8daffaf64a77870b0e44421
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-09-22 13:32:27 -0600

    Minor changes throughout

--- scripts/app.js
+++ scripts/app.js
@@ -15,48 +15,71 @@ function playGame() {
     let startPlayerMarginRight = 339;
     let startPlayerMarginLeft = 339;
 
-    let increment = 0; //defined outside function so the value can accumulate
-    let incrementMiddle = 0;
+    let incrementLeft = 0; //defined outside function so the value can accumulate
+    let incrementMiddleLeft = 0;
+    let incrementRight = 0;
+    let incrementMiddleRight = 0;
 
     //Controls motion of obstacles, hit detection, and detemines if player has reached end zone
-    function moveBlock() {
+    function moveBlockLeft() {
 
-        increment += 2;
-        incrementMiddle += 2;
+        incrementLeft += 2;
 
-        $(".motionLeft").css({ marginRight: `${increment}px`, marginLeft: `${660-increment}px` }); //had difficulty with syntax of template literals in this case
-        $(".motionRight").css({ marginRight: `${660-increment}px`, marginLeft: `${increment}px` }); 
+        $(".motionLeft").css({ marginRight: `${-60+incrementLeft}px`, marginLeft: `${720-incrementLeft}px` }); //had difficulty with syntax of template literals in this case
 
-        $(".motionLeftOffset").css({ marginRight: `${360 + incrementMiddle}px`, marginLeft: `${300-incrementMiddle}px` });
-        $(".motionRightOffset").css({ marginRight: `${300-incrementMiddle}px`, marginLeft: `${360 + incrementMiddle}px` }); 
-
-        if (increment >= 720) { //resets increment value when obstacle leaves game area, creating appearance that it looped back to original location
-            increment = 0;
+        if (incrementLeft >= 780) { //resets increment value when obstacle leaves game area, creating appearance that it looped back to original location
+            incrementLeft = 0;
         }
 
-        if (incrementMiddle >= 360) {
-            incrementMiddle = -360;
-        }
-
-        if (playerX < (increment + obstacleWidth) && (playerX + playerWidth) > increment && 
+        if (playerX < (incrementLeft - 60 + obstacleWidth) && (playerX + playerWidth) > (incrementLeft-60) && 
             (startRow === 9 || startRow === 7 || startRow === 5 || startRow === 3)) {
             console.log("hit from right");
             lostLife();
         }
 
-        if (playerX < (incrementMiddle + obstacleWidth + 360) && (playerX + playerWidth) > (incrementMiddle + 360) && 
+        //If player reaches end zone, game is over, and winGame() is executed
+        if (startRow === 1) {
+            winGame();
+        }
+    }
+
+    setInterval(moveBlockLeft, 4);
+
+    function moveBlockMiddleLeft() {
+
+        incrementMiddleLeft += 2;
+
+        $(".motionLeftOffset").css({ marginRight: `${360 + incrementMiddleLeft}px`, marginLeft: `${300-incrementMiddleLeft}px` });
+
+        if (incrementMiddleLeft >= 360) {
+            incrementMiddleLeft = -420;
+        }
+
+        if (playerX < (incrementMiddleLeft + obstacleWidth + 360) && (playerX + playerWidth) > (incrementMiddleLeft + 360) && 
             (startRow === 9 || startRow === 7 || startRow === 5 || startRow === 3)) {
             console.log("hit from right");
             lostLife();
         }
 
-        if (playerX < ((660-increment)+obstacleWidth) && (playerX + playerWidth) > (660-increment) && 
-            (startRow === 8 || startRow === 6 || startRow === 4 || startRow === 2)) {
-            console.log("hit from left");
-            lostLife();
+        //If player reaches end zone, game is over, and winGame() is executed
+        if (startRow === 1) {
+            winGame();
+        }
+    }
+
+    setInterval(moveBlockMiddleLeft, 7);
+
+    function moveBlockRight() {
+
+        incrementRight += 2;
+
+        $(".motionRight").css({ marginRight: `${720-incrementRight}px`, marginLeft: `${-60+incrementRight}px` }); 
+
+        if (incrementRight >= 780) { //resets increment value when obstacle leaves game area, creating appearance that it looped back to original location
+            incrementRight = 0;
         }
 
-        if (playerX < ((300-incrementMiddle)+obstacleWidth) && (playerX + playerWidth) > (300-incrementMiddle) && 
+        if (playerX < ((720-incrementRight)+obstacleWidth) && (playerX + playerWidth) > (720-incrementRight) && 
             (startRow === 8 || startRow === 6 || startRow === 4 || startRow === 2)) {
             console.log("hit from left");
             lostLife();
@@ -68,7 +91,31 @@ function playGame() {
         }
     }
 
-    setInterval(moveBlock, 30);
+    setInterval(moveBlockRight, 5);
+
+    function moveBlockMiddleRight() {
+
+        incrementMiddleRight += 2;
+
+        $(".motionRightOffset").css({ marginRight: `${300-incrementMiddleRight}px`, marginLeft: `${360 + incrementMiddleRight}px` }); 
+
+        if (incrementMiddleRight >= 360) {
+            incrementMiddleRight = -420;
+        }
+
+        if (playerX < ((300-incrementMiddleRight)+obstacleWidth) && (playerX + playerWidth) > (300-incrementMiddleRight) && 
+            (startRow === 8 || startRow === 6 || startRow === 4 || startRow === 2)) {
+            console.log("hit from left");
+            lostLife();
+        }
+
+        //If player reaches end zone, game is over, and winGame() is executed
+        if (startRow === 1) {
+            winGame();
+        }
+    }
+
+    setInterval(moveBlockMiddleRight, 4);
 
     //Defines what occurs when player wins game
     function winGame() {
@@ -78,6 +125,8 @@ function playGame() {
         startPlayerMarginLeft = 339;
 
         $("#gameContainer").css({display: "none"});
+        $(".Win-Message").text(`You Win! You completed the game in ${timer} seconds!`)
+        console.log(timer);
         $(".Win-Message").css({display: "block"})
         $(".playAgain").css({display: "block"});
         lives = 3;
@@ -114,12 +163,14 @@ function playGame() {
                 if (startRow === 10) {
                     break;
                 }
+                console.log("down");
                 $(".player").css({gridRow: `${startRow = startRow + 1}`});
                 break;
             case "ArrowUp":
                 if (startRow === 1) {
                     break;
                 }
+                console.log("up");
                 $(".player").css({gridRow: `${startRow = startRow - 1}`});
                 break;
             case "ArrowLeft":
@@ -127,6 +178,7 @@ function playGame() {
                     lostLife();
                     break;
                 }
+                console.log("left");
                 $(".player").css({marginRight: `${startPlayerMarginRight += 52}px`, marginLeft: `${startPlayerMarginLeft -= 52}px`});
                 break;
             case "ArrowRight":
@@ -134,6 +186,7 @@ function playGame() {
                     lostLife();
                     break;
                 }
+                console.log("right");
                 $(".player").css({marginRight: `${startPlayerMarginRight -= 52}px`, marginLeft: `${startPlayerMarginLeft += 52}px`});
 
                 break;
@@ -176,4 +229,57 @@ $(".playAgain").on("click", function () {
 
     playGame();
 });
+
+//Old motion function with all obstacles combined:
+
+   // function moveBlock() { //This combines the motion functions into one; however, only one speed can be chosen for all blocks
+
+    //     increment += 2;
+    //     incrementMiddle += 2;
+
+    //     $(".motionLeft").css({ marginRight: `${-60+increment}px`, marginLeft: `${720-increment}px` }); //had difficulty with syntax of template literals in this case
+    //     $(".motionRight").css({ marginRight: `${720-increment}px`, marginLeft: `${-60+increment}px` }); 
+
+    //     $(".motionLeftOffset").css({ marginRight: `${360 + incrementMiddle}px`, marginLeft: `${300-incrementMiddle}px` });
+    //     $(".motionRightOffset").css({ marginRight: `${300-incrementMiddle}px`, marginLeft: `${360 + incrementMiddle}px` }); 
+
+    //     if (increment >= 780) { //resets increment value when obstacle leaves game area, creating appearance that it looped back to original location
+    //         increment = 0;
+    //     }
+
+    //     if (incrementMiddle >= 360) {
+    //         incrementMiddle = -420;
+    //     }
+
+    //     if (playerX < (increment - 60 + obstacleWidth) && (playerX + playerWidth) > (increment-60) && 
+    //         (startRow === 9 || startRow === 7 || startRow === 5 || startRow === 3)) {
+    //         console.log("hit from right");
+    //         lostLife();
+    //     }
+
+    //     if (playerX < (incrementMiddle + obstacleWidth + 360) && (playerX + playerWidth) > (incrementMiddle + 360) && 
+    //         (startRow === 9 || startRow === 7 || startRow === 5 || startRow === 3)) {
+    //         console.log("hit from right");
+    //         lostLife();
+    //     }
+
+    //     if (playerX < ((720-increment)+obstacleWidth) && (playerX + playerWidth) > (720-increment) && 
+    //         (startRow === 8 || startRow === 6 || startRow === 4 || startRow === 2)) {
+    //         console.log("hit from left");
+    //         lostLife();
+    //     }
+
+    //     if (playerX < ((300-incrementMiddle)+obstacleWidth) && (playerX + playerWidth) > (300-incrementMiddle) && 
+    //         (startRow === 8 || startRow === 6 || startRow === 4 || startRow === 2)) {
+    //         console.log("hit from left");
+    //         lostLife();
+    //     }
+
+    //     //If player reaches end zone, game is over, and winGame() is executed
+    //     if (startRow === 1) {
+    //         winGame();
+    //     }
+    // }
+
+    // setInterval(moveBlock, 50);
 
